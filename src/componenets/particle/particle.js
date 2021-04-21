23ae915f25d63d578e66a513e4c692b86e45f7db
vaibhav saini <vbsaini2004@gmail.com>
2021-04-21 11:23:22 +0530

    fixed a typo

--- src/componenets/particle/particle.js
+++ src/componenets/particle/particle.js
@@ -104,6 +104,84 @@ function Particle() {
             },
         };
     }
+    if (document.body.clientWidth < 700) {
+        options = {
+            particles: {
+                number: {
+                    value: 40,
+                },
+                color: "#ffffff",
+                size: {
+                    value: 4.3,
+                },
+                line_linked: {
+                    color: "rgb(0,0,128)",
+                    opacity: 0.5,
+                },
+            },
+            interactivity: {
+                detectsOn: "canvas",
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                },
+            },
+        };
+    }
+    if (document.body.clientWidth < 600) {
+        options = {
+            particles: {
+                number: {
+                    value: 30,
+                },
+                color: "#ffffff",
+                size: {
+                    value: 4.3,
+                },
+                line_linked: {
+                    color: "rgb(0,0,128)",
+                    opacity: 0.5,
+                },
+            },
+            interactivity: {
+                detectsOn: "canvas",
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                },
+            },
+        };
+    }
+    if (document.body.clientWidth < 500) {
+        options = {
+            particles: {
+                number: {
+                    value: 20,
+                },
+                color: "#ffffff",
+                size: {
+                    value: 4.3,
+                },
+                line_linked: {
+                    color: "rgb(0,0,128)",
+                    opacity: 0.5,
+                },
+            },
+            interactivity: {
+                detectsOn: "canvas",
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                },
+            },
+        };
+    }
 
     return (
         <div className="particle" id="particle">
